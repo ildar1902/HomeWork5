3d7fa9d78b8b694dd5536257fe023e52b9306a79
Home <ildar86@live.ru>
2022-11-08 14:37:01 +0500

    ДЗ 5. Выполнено задание 1.

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Дз-5. Задание 1.");
+
+        // Задание 1.
+        int clientOs = 5;
+        if (clientOs == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке"); }
+        else if (clientOs == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке"); }
+        else {
+            System.out.println("Для данной ОС у нас пока нет мобильного приложения"); }
+
+        //Задание 2.
+
+    }
+}
